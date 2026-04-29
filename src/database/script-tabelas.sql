5e9881f9cab7b71824fc5b86e53e679e4e446c1f
GuilhermeAlbuquerque501 <guilherme.albuquerque@sptech.school>
2026-04-28 22:24:58 -0300

    feat: adequando a api ao projeto para o login e cadastro

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -72,7 +72,7 @@ CREATE TABLE resposta_usuario (
 		REFERENCES alternativa(id_alternativa)
 );
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+-- insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
+-- insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
+-- insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+-- insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
