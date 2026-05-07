c05b37129af1bce142cc025755c6e44dc300c4ae
guilhermealbuquerque501 <guilherme.albuquerque@sptech.shcool>
2026-05-07 19:31:30 -0300

    feat: login finalizado, refactor: refatorando bd e modelagem, idealizando o quiz, feat: bd na VM funcionando

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -16,27 +16,6 @@ CREATE TABLE quiz (
     descricao VARCHAR(255)
 );
 
-CREATE TABLE pergunta (
-    id_pergunta INT PRIMARY KEY AUTO_INCREMENT,
-    texto VARCHAR(255) NOT NULL,
-    fk_quiz INT,
-    
-    CONSTRAINT cFkQuiz
-    FOREIGN KEY (fk_quiz) 
-		REFERENCES quiz(id_quiz)
-);
-
-CREATE TABLE alternativa (
-    id_alternativa INT PRIMARY KEY AUTO_INCREMENT,
-    texto VARCHAR(255) NOT NULL,
-    correta BOOLEAN NOT NULL,
-    fk_pergunta INT,
-    
-    CONSTRAINT	cFk_pergunta
-    FOREIGN KEY (fk_pergunta) 
-		REFERENCES pergunta(id_pergunta)
-);
-
 CREATE TABLE resultado_quiz (
     id_resultado INT PRIMARY KEY AUTO_INCREMENT,
     pontuacao INT,
@@ -46,33 +25,7 @@ CREATE TABLE resultado_quiz (
     
     CONSTRAINT cFkUsuario
     FOREIGN KEY (fk_usuario) 
-		REFERENCES usuario(id_usuario),
-        
-	CONSTRAINT cFkQuiz2
-    FOREIGN KEY (fk_quiz) 
-		REFERENCES quiz(id_quiz)
+		REFERENCES usuario(id_usuario)
 );
 
-CREATE TABLE resposta_usuario (
-    id_resposta INT PRIMARY KEY AUTO_INCREMENT,
-    fk_usuario INT,
-    fk_pergunta INT,
-    fk_alternativa INT,
-    
-    CONSTRAINT cFkUsuario2
-    FOREIGN KEY (fk_usuario) 
-		REFERENCES usuario(id_usuario),
-        
-	CONSTRAINT cFkPergunta
-    FOREIGN KEY (fk_pergunta) 
-		REFERENCES pergunta(id_pergunta),
-        
-	CONSTRAINT cFkAlternativa
-    FOREIGN KEY (fk_alternativa) 
-		REFERENCES alternativa(id_alternativa)
-);
 
--- insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
--- insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
--- insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
--- insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
